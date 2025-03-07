06f84555b92d7175fcb01da7aacc1682457b3a66
farelkdhfi <farelkadhafi34@gmail.com>
2025-03-07 15:17:13 +0700

    ljljl

--- src/homepages/LogoCarousel.jsx
+++ src/homepages/LogoCarousel.jsx
@@ -13,7 +13,7 @@ const LogoCarousel = () => {
     useEffect(() => {
         const marquee = marqueeRef.current;
         const clone = marquee.innerHTML;
-        marquee.innerHTML += clone; // Duplicate content for seamless loop
+        marquee.innerHTML += clone + clone + clone + clone + clone; // Duplicate content for seamless loop
 
         tweenRef.current = gsap.to(marquee, {
             x: "-50%", // Move halfway to create an infinite loop
@@ -34,7 +34,7 @@ const LogoCarousel = () => {
     return (
         <div className='px-3 lg:px-0 w-full mx-auto'>
             <div className="overflow-hidden bg-white py-10 md:py-12" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <div ref={marqueeRef} className="flex gap-x-6 sm:gap-x-8 md:gap-x-10 whitespace-nowrap">
+                <div ref={marqueeRef} className="flex space-x-6 sm:space-x-8 md:space-x-10 whitespace-nowrap">
                     <img src={Img5} alt="Logo 1" className="w-24 sm:w-28 md:w-32 h-auto" />
                     <img src={Img6} alt="Logo 2" className="w-24 sm:w-28 md:w-32 h-auto" />
                     <img src={Img7} alt="Logo 3" className="w-24 sm:w-28 md:w-32 h-auto" />
