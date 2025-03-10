45d4313777a7b6ec41e8c0b050b48f3fff07e3a9
farelkdhfi <farelkadhafi34@gmail.com>
2025-03-11 03:06:04 +0700

    lala

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
   return (
     <section className='pt-20 md:pt-30 px-3 lg:px-15 bg-white flex justify-center items-center min-h-screen'>
         <div className='flex flex-col space-y-2 justify-center items-center'>
-            <img src={Logo} alt="" />
-            <p className='text-sm'>Presented for WDC competition 2025</p>
+            <img src={Logo} alt="" className='w-20 h-auto' />
+            <p className='text-xs text-pretty max-w-3xs text-center'>Presented for WDC competition 2025</p>
         </div>
     </section>
   )
